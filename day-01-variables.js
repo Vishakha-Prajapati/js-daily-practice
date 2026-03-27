2e1a7c6c0dd22ec547d9fcd9c6c2347b2819b926
vishakhaprajapati <vishakhaprajapati0502@gmail.com>
2026-03-27 15:22:43 +0530

    Day 1: JS basics - variables, mutation, conditionals, loops

---
+++ day-01-variables.js
@@ -0,0 +1,16 @@
+// Variables: var, let, const
+
+let name = "Vishakha";
+const age = 25;
+
+name = "Dev"; // reassignment works
+
+// const age = 30 ❌ not allowed
+// let x = 5;
+// let y = x;
+
+// y = 10;
+
+// console.log(x);
+// console.log(y);
+// console.log(x);
