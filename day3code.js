f519eab7a7cfbb7b3791c0894f508fef71aa438b
vishakhaprajapati <vishakhaprajapati0502@gmail.com>
2026-04-08 13:58:38 +0530

    Day 3: JS basics - Loops-ForLoop and WhileLoop

--- day3code.js
+++ day3code.js
@@ -1,31 +1,42 @@
-// console.log("ram")
-// console.log("ram")
-// console.log("ram")
-// console.log("ram")
-// console.log("ram")
+// console.log("vishakha");
+// console.log("vishakha");
+// console.log("vishakha");
+// console.log("vishakha");
+// console.log("vishakha");
 
-// For loop
+// For Loop 
+//  for (let i = 0 ;i<10;i++){
+//     console.log("vishakha");
+//  }
 
+ // how it works 
+//   i =0  -> 0<10 -> true -> print vishakha -> i++ -> means i+1 -> i=1
+//   i =1  -> 1<10 -> true -> print vishakha -> i++ -> i=2
+//   i =2  -> 2<10 -> true -> print vishakha -> i++ -> i=3
+//..
+//   i =9  -> 9<10 -> true -> print vishakha -> i++ -> i=10
+//   i =10  -> 10<10 -> false -> loop ends
 
-for (let  i =0; i<100;i++){
-    console.log(" loop ram")
-}
+//  for (let i=1; i<=100;i++){
+//     console.log(i);
+//  }
 
-// i=0 - initialization
-// i<100 - condition
-// i++ - increment
- //i =0  0<100 true i++ it means i=i+1  i=1
- //i =1  1<100 true i++ it means i=i+1  i=2
- //i =2  2<100 true i++ it means i=i+1  i=3
- //i =99  99<100 true i++ it means i=i+1  i=100
- //i =100  100<100 false loop will stop
-//   for (let  i = 0; i<=100;i++){
-//     console.log(i)
+// for(let i=10; i>=1; i--){
+//     console.log(i);
 // }
+// how it works
+//   i =10  -> 10>=1 -> true -> print 10 -> i-- -> means i-1 -> i=9
+//   i =9  -> 9>=1 -> true -> print 9 -> i-- -> i=8
+//   i =8  -> 8>=1 -> true -> print 8 -> i-- -> i=7
+//..
+//   i =2  -> 2>=1 -> true -> print 2 -> i-- -> i=1
+//   i =1  -> 1>=1 -> true -> print 1 -> i-- -> i=0
+//   i =0  -> 0>=1 -> false -> loop ends
 
-// while loop
-let i = 0; // initialization
-while (i < 100) { // condition
-    console.log("while loop ram");
-     i++; // increment
+//while Loop
+ 
+let i =1;
+while(i<=10){
+    console.log(i);
+    i++;
 }
